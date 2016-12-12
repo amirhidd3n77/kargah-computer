565f4ba60cea94e08e5d0bb0e18380bd84298925
amirhidd3n77 <amirhidd3n77@gmail.com>
2016-12-11 23:18:38 -0800

    Add files via upload

---
+++ HW5#1.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+int donbale (int n)
+{
+    if (n==0)
+        return -1;
+    if (n==1)
+        return 1;
+    if (n==2)
+        return 3;
+    else
+        return donbale(n-1) + donbale(n-2) + n;
+
+}
+int check_donbale (int x)
+{
+    int i=1;
+        while (donbale(i)<=x)
+        {
+            if (donbale(i)==x)
+             {
+
+                return i;
+                break;
+             }
+
+
+             i++;
+
+
+        }
+
+    return -1;
+
+}
+int main ()
+{
+    int a,b,i;
+    scanf("%d%d",&a,&b);
+    for (i=a;i<=b;i++)
+    {
+        if (check_donbale(i)!=-1)
+            printf("%d @ %d\n" ,i,check_donbale(i));
+    }
+    return 0;
+}
+
+
+
+
+
+
+
